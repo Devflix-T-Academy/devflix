11f8dc9e15614f3bb37404cae02d18f508b3e5e4
Paulo Henrique <ph.1928@hotmail.com>
2024-07-12 10:23:34 -0300

    WIP: user controller methods

--- src/repository/GeneralRepository.java
+++ src/repository/GeneralRepository.java
@@ -5,7 +5,6 @@ import model.User;
 import java.util.Optional;
 
 public interface GeneralRepository {
-    public Optional<User> findByEmail(String email);
     public void create(User user);
     public void update(User user);
     public void delete(Long id);
